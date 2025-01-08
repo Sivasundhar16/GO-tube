8c544adb3f0acf0a4cf18445085bf3377e93e3f4
Sivasundhar16 <vssiva18@gmail.com>
2025-01-08 20:47:24 +0530

    redux toolkit working correctly

--- src/components/Head.jsx
+++ src/components/Head.jsx
@@ -1,8 +1,18 @@
+import { useDispatch } from "react-redux";
+import { toggleMenu } from "../utils/navSlice";
+
 export const Head = () => {
+  const dispatch = useDispatch();
+
+  const togglemenuHandler = () => {
+    dispatch(toggleMenu());
+  };
+
   return (
     <div className="grid grid-flow-col items-center my-2 shadow-lg">
       <div className="flex items-center">
         <img
+          onClick={() => togglemenuHandler()}
           className="h-5 m-3 col-span-3"
           src="https://t3.ftcdn.net/jpg/01/09/45/80/240_F_109458015_QsWmchlzuwCZPqIUWR7HcTDsbbptejRv.jpg"
           alt="menu"
@@ -10,11 +20,13 @@ export const Head = () => {
       </div>
 
       <div className="flex items-center justify-center col-span-6 space-x-0">
-        <img
-          className="h-12"
-          src="https://imgs.search.brave.com/TgMT524wUGVW17rXOu4J2JqpVl7tl2SeEjtrYnyFCgg/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly90NC5m/dGNkbi5uZXQvanBn/LzAyLzEwLzEwLzI5/LzM2MF9GXzIxMDEw/Mjk2NV9JSjA3YldV/UUN2VHR2bkN4UGNV/WjZtanJsUWdsQjZy/Si5qcGc"
-          alt="Goutube Logo"
-        />
+        <a href="/">
+          <img
+            className="h-12"
+            src="https://imgs.search.brave.com/TgMT524wUGVW17rXOu4J2JqpVl7tl2SeEjtrYnyFCgg/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly90NC5m/dGNkbi5uZXQvanBn/LzAyLzEwLzEwLzI5/LzM2MF9GXzIxMDEw/Mjk2NV9JSjA3YldV/UUN2VHR2bkN4UGNV/WjZtanJsUWdsQjZy/Si5qcGc"
+            alt="Goutube Logo"
+          />
+        </a>
         <input
           className="h-8 text-center rounded-l-full w-72 uppercase bg-gray-300 border-none outline-none"
           type="text"
